c4624e1ac1d6661ac9c77fb251a5d2d956be8cf0
Mohamed Riyas Rahuman M <riyas03524@gmail.com>
2025-10-02 15:49:42 +0530

    delete reset_db.py

--- reset_db.py
+++ reset_db.py
@@ -1,9 +1,1 @@
-import os
-from app import app, db
 
-with app.app_context():
-    # Drop all tables
-    db.drop_all()
-    # Create all tables with new schema
-    db.create_all()
-    print("✅ Database reset successfully!")
